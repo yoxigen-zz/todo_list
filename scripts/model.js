3d9195126f3bf11c922c40e690a9308268e725aa
unknown <idanp@idanp2-PC.Fortscale.dom>
2015-02-24 11:48:26 +0200

    FINISH TO DEVELOPE THE SAVE TODO

--- scripts/model.js
+++ scripts/model.js
@@ -1,14 +1,27 @@
 (function(){ 
+
+    //Will represent if the filter is turn on or off
     var filterIsOn = false;
+
+    //The HashMap of the todos
     var toDosMap = new Map();
+
+    //The id of the todos
     var id =1;
 
+
+//The Dictionary that collect bad words
     var Dictionary ={
        lastUpdateDate : "22-02-2015",
        badWords : ["Fuck","Fucker","Blat"]
     }
 
+//Thethod the decide if text iss appropriate 
     function isExplicit (toDoText){
+
+        if(!filterIsOn)
+            return false;
+
         return Dictionary.badWords.some(function (item){
             return toDoText.toLowerCase().indexOf(item.toLowerCase()) > -1
         });
@@ -26,8 +39,23 @@
         saveToDos: function(taskText){
             var currentTime = new Date().getTime();
             
+            var explicitFlag = isExplicit(taskText);
+
+            var toDoInstance = new ToDo(taskText,currentTime,explicitFlag);
             
-            alert(isExplicit("need to eat the diner with Mother Fucker Bitch Blat"));
+            toDosMap.set(id,toDoInstance);
+            
+
+               var p1 = dal.setAllTodos(toDoInstance);
+               p1.then(function() {
+                                    id++;
+                                    console.log("The ToDo task was successfuly persisted"); // "Stuff worked!"
+                }, function() {
+                    toDosMap.get(id);
+                    console.log("The ToDo can't be saved please try in a copule of seconds"); // Error: "It broke"
+                });
+
+            //alert("the to do id is:"+(id-1)+"text: "+toDosMap.get(id-1).toDo+" date : "+toDosMap.get(id-1).date+" explicit is: "+toDosMap.get(id-1).isExplicit);
         },
 
         deleteToDo: function(){
