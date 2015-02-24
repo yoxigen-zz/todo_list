db05d7552d1783d2ce114d423a65b6e2ffc5cd8e
unknown <idanp@idanp2-PC.Fortscale.dom>
2015-02-24 10:56:49 +0200

    add Dictonary and isExplicit method

--- scripts/model.js
+++ scripts/model.js
@@ -1,6 +1,23 @@
 (function(){ 
     var filterIsOn = false;
 
+    var Dictionary ={
+       lastUpdateDate : "22-02-2015",
+       badWords : ["Fuck","Fuck you","Fucker","Blat"]
+    }
+
+    isExplicit : function (toDoText){
+        for each (var item in Dictionary.badWords)
+        {
+            if (toDoText.toLowerCase().indexOf(item.toLowerCase()) > -1)
+                return 1;
+        }
+
+        return 0;
+    }
+
+    
+
     var model = {
         getToDos: function(){
             return [];
