83ac1c404984c6d231d3aa7413d687e13e18bf7c
unknown <rotemn@rotem-lap.Fortscale.dom>
2015-02-24 11:09:22 +0200

    Add dal functionality (not finished yet)

--- scripts/dal.js
+++ scripts/dal.js
@@ -1,27 +1,40 @@
 (function() {
 
+    // name of local storage
+    var localStorageKey = "Fun!";
+
     var dal = {
 
+        // Return a list of all tasks in the local storage (as promise)
         getAllTodos : function() {
 
-            // TODO get string from local storage
+            // get string from local storage
+            var objectsString = localStorage.getItem(localStorageKey);
 
-            // TODO deserialize string to list of objects
+            // deserialize string to list of objects
+            var objectsJson = JSON.parse(objectsString);
 
             // TODO return promise for all data
-        }
+            return objectsJson;
+        },
 
-
+        // save the list of all tasks in the local storage
         setAllTodos : function(mapOfObjects) {
 
-            // TODO serialize list of objects to string
+            // serialize list of objects to string
+            var objectsString = JSON.stringify(mapOfObjects);
 
-            // TODO save string to local storage
+            // save string to local storage
+            localStorage.setItem(localStorageKey, objectsString);
 
             // TODO return promise for success/failure
         }
 
     };
+
+
+    // make the DAL avaiable from outside of this scope
+    window.dal = dal;
 
 
 })();
