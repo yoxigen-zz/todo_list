3abfe85bb6c29c2aaa9fa67dc4b98df183a07a40
unknown <rotemn@rotem-lap.Fortscale.dom>
2015-02-24 10:38:41 +0200

    Add dal for localstorage

---
+++ scripts/dal.js
@@ -0,0 +1,27 @@
+(function() {
+
+    var dal = {
+
+        getAllTodos : function() {
+
+            // TODO get string from local storage
+
+            // TODO deserialize string to list of objects
+
+            // TODO return promise for all data
+        }
+
+
+        setAllTodos : function(mapOfObjects) {
+
+            // TODO serialize list of objects to string
+
+            // TODO save string to local storage
+
+            // TODO return promise for success/failure
+        }
+
+    };
+
+
+})();
