6a4d74755fee89d1bd6da948fe657c62639aed03
Lior Govrin <liorg@fortscale.com>
2015-03-09 10:54:53 +0200

    getToDos

--- ng-scripts/services/model.js
+++ ng-scripts/services/model.js
@@ -1,16 +1,30 @@
 (function(){
     angular.module("FunTodo").factory("model", model);
-
     model.$inject = ["DAL"];
 
-
     function model(DAL){
-        var modelApi = {
+        // Boolean indicating if explicit words are filtered or not
+        var filterIsOn = false;
+        var toDosMap;
 
+        var modelApi = {
+            getToDos : function(){
+                if(!filterIsOn){
+                    return new Promise(function(resolve){
+                        resolve(toDosMap);
+                    });
+                }
+
+                return new Promise(function(resolve){
+                    var retToDos =
+                });
+            }
+
+            updateFilter : function(switchOn){
+                filterIsOn = switchOn;
+            }
         }
 
         return modelApi;
-
-
     }
 })();
