fe2246b3c32979a6b9d129e36a33688de5ee1ae8
Lior Govrin <liorg@fortscale.com>
2015-03-09 11:09:13 +0200

    finish get ToDos

--- ng-scripts/services/model.js
+++ ng-scripts/services/model.js
@@ -5,36 +5,28 @@
     function model(DAL){
         // Boolean indicating if explicit words are filtered or not
         var filterIsOn = false;
-        var toDosMap;
-
-
-        //Will represent if the filter is turn on or off
-        var filterIsOn = false;
         var toDosMap = getFromStorage();
 
-        //The id of the todos
-        var id =1;
+        // The id of the toDos
+        var id = 1;
 
-
-        //The Dictionary that collect bad words
-        var Dictionary ={
+        // The Dictionary that collect bad words
+        var Dictionary = {
             lastUpdateDate : "22-02-2015",
             badWords : ["Fuck","Fucker","Blat"]
         };
 
-
         function getFromStorage(){
             var promise = dal.getAllTodos();
-            return promise.then(function(result) {
+            return promise.then(function(result){
                 return result;
-            }, function(err) {
+            }, function(err){
                 console.log(err);
             });
         }
 
-       //Thethod the decide if text iss appropriate
+        // Method to decide if text is appropriate
         function isExplicit (toDoText){
-
             if(!filterIsOn)
                 return false;
 
@@ -52,9 +44,15 @@
                 }
 
                 return new Promise(function(resolve){
-                    var retToDos =
+                    var retToDos = new Map();
+                    toDosMap.forEach(function(value, key){
+                        if(!value.isExplicit){
+                            retToDos.set(key, value);
+                        }
+                    });
+                    resolve(retToDos);
                 });
-            }
+            },
 
             updateFilter : function(switchOn){
                 filterIsOn = switchOn;
