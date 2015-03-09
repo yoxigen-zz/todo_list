99feda91bc58ad0ad7cbaf1a843bf70d248e427d
Galia <galiar@fortscale.com>
2015-03-09 10:48:33 +0200

    ng-script/dal.js

--- ng-scripts/services/dal.js
+++ ng-scripts/services/dal.js
@@ -1,5 +1,53 @@
 angular.module("FunTodo").factory("DAL", function(){
     var dalApi = {};
+// Return a list of all tasks in the local storage (as promise)
+
+        var localStorageKey = "Fun!";
+
+        getAllTodos : function() {
+
+            // create promise for all data from local storage
+            var promise = new Promise(function(resolve, reject) {
+
+                // get string from local storage
+                var objectsString = localStorage.getItem(localStorageKey);
+
+                // deserialize string to list of objects
+                var objectsJson = JSON.parse(objectsString);
+
+                // on success
+                resolve(objectsJson);
+
+            });
+
+            // return promise for all data
+            return promise;
+        },
+
+
+
+        // save the list of all tasks in the local storage
+        setAllTodos : function(mapOfObjects) {
+
+            // create promise for all data from local storage
+            var promise = new Promise(function(resolve, reject) {
+
+                // serialize list of objects to string
+                var objectsString = JSON.stringify(mapOfObjects);
+
+                // save string to local storage
+                localStorage.setItem(localStorageKey, objectsString);
+
+                // on success
+                resolve();
+
+            });
+
+            // return promise for success/failure
+            return promise;
+        }
+
+
 
     return dalApi;
 });
