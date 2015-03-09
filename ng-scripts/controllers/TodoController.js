993ef3c6db47bc5fbdbad9c8540682f525c9ec5c
Dana Livni <danal@fortscale.com>
2015-03-09 11:41:11 +0200

    add view part

--- ng-scripts/controllers/TodoController.js
+++ ng-scripts/controllers/TodoController.js
@@ -2,9 +2,38 @@
 
     angular.module("FunTodo").controller("TodoController", TodoController);
 
-    TodoController.$inject = ["$scope"];
+    TodoController.$inject = ["$scope","model"];
 
-    function TodoController($scope){
-        $scope.test = "Danger Danger Ran Door!";
+
+    function TodoController($scope,model){
+            refreshToDoList();
+
+            $scope.deleteToDo =  function (id) {
+                 model.deleteToDo(id);
+                 refreshToDoList();
+            };
+
+            $scope.addToDo = function () {
+                if($scope.newToDo != ""){
+                    model.saveToDos($scope.newToDo);
+                    refreshToDoTable();
+                    $scope.newToDo = "";
+                }
+            };
+
+            $scope.updateFilter = function () {
+                model.updateFilter($scope.filter);
+                refreshToDoTable();
+            };
+
+            function refreshToDoList(){
+               var p = model.getToDos();
+                p.then(function (toDos) {
+                 $scope.toDoList = toDos;
+                }, function () {
+                    console.log("Problem getting the ToDo list"); // Error: "It broke"
+                });
+                //$scope.toDoList =[{ "id": 1, "text" : "todo1" , "date": "724901741920"},{"id": 2, "text" : "todo2" , "date": "724901741920"}];
+            }
     }
 })();
